a56031db0c7e958c97787afb56760e8fa308e52d
abdul <abdulrahuman380525@gmail.com>
2025-11-20 14:46:57 +0530

    first commit

---
+++ pp1.py
@@ -0,0 +1,11 @@
+# Dictionary program: Store and print student marks
+
+student_marks = {
+    "Abdul": 95,
+    "Rahuman": 88,
+    "Shivanya": 92
+}
+
+# Printing keys and values
+for name, mark in student_marks.items():
+    print(name, "scored", mark)
